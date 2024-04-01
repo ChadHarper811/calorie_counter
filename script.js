ac63631d6fc75e095f672c9b6b8aee87c8e32c32
Chad Harper <chadharper811@gmail.com>
2024-03-31 21:41:41 -0500

    step 62

--- script.js
+++ script.js
@@ -18,5 +18,27 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll();
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+        <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+        <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"/>
+        <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+        <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />
+        `;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+    }
+}
+
+addEntryButton.addEventListener("click", addEntry);
